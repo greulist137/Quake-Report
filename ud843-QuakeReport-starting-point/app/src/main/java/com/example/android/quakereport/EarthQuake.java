03c7cbd913b793faf0d2753b2d66525e67c6047e
Steven Greulich <greulich.steven@gmail.com>
2018-05-07 17:48:57 -0700

    commit - creation of EarthQuake class
    The purpose of this commit is for the base creation of the earthquake class:
    1) base class creation
    2) constructor creation

---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthQuake.java
@@ -0,0 +1,14 @@
+package com.example.android.quakereport;
+
+public class EarthQuake {
+
+    private double mMagnitude;
+    private String mCityOfEarthquake;
+    private String mDateOfEarthquake;
+
+    public EarthQuake(double magnitude, String cityOfEarthquake, String dateOfEarthquake){
+        mMagnitude = magnitude;
+        mCityOfEarthquake = cityOfEarthquake;
+        mDateOfEarthquake = dateOfEarthquake;
+    }
+}
